5cb3d436777527fec45f661416d03a3e4faff8ea
Daniel Smith <smithdaniel@web.de>
2021-09-26 13:57:05 +0200

    Fixed countdown

--- lib/pages/save_the_date/countdown.dart
+++ lib/pages/save_the_date/countdown.dart
@@ -9,7 +9,8 @@ class ShowCountdownTimer extends StatefulWidget {
 class _ShowCountdownTimerState extends State<ShowCountdownTimer> {
   CountdownTimerController controller;
   int endTime =
-      DateTime.parse("2021-10-14 12:00:00").millisecondsSinceEpoch + 1000 * 30;
+      DateTime.parse("2021-10-14 00:00:00").millisecondsSinceEpoch + 1000 * 30;
+  int endTimeTimestamp = 1634162400000;
 
   @override
   void initState() {
@@ -34,63 +35,36 @@ class _ShowCountdownTimerState extends State<ShowCountdownTimer> {
         CountdownTimer(
           controller: controller,
           widgetBuilder: (_, CurrentRemainingTime time) {
-            if (time == null) {
+            if (DateTime.now().millisecondsSinceEpoch > endTimeTimestamp) {
               return Text(
                 'Endlich ist es soweit!',
                 textAlign: TextAlign.center,
                 style: Theme.of(context).textTheme.headline5,
               );
+            } else if (time.days >= 2) {
+              return Text(
+                'In ${(time.days + 1).toString()} Tagen ist es soweit!',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline5,
+              );
+            } else if (time.days < 2 && time.hours < 2) {
+              return Text(
+                'Morgen ist es endlich soweit!',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline5,
+              );
+            } else if (time.days < 2 && time.hours >= 2) {
+              return Text(
+                'In ${(time.days + 1).toString()} Tagen ist es soweit!',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline5,
+              );
             } else {
-              if (time.days == null && time.hours != null) {
-                return Column(
-                  children: [
-                    Text(
-                      'In ${time.hours.toString()} Stunden ist es soweit!',
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.headline5,
-                    ),
-                  ],
-                );
-              }
-              if (time.days == null && time.hours == null && time.min != null) {
-                if (time.min < 2) {
-                  return Column(
-                    children: [
-                      Text(
-                        'In einer Minute ist es soweit!',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                    ],
-                  );
-                } else {
-                  return Column(
-                    children: [
-                      Text(
-                        'In ${time.min.toString()} Minuten ist es soweit!',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                    ],
-                  );
-                }
-              }
-              if (endTime <= DateTime.now().millisecondsSinceEpoch) {
-                return Container();
-              }
-              if (time.days == 1) {
-                return Text(
-                  'Morgen ist es soweit!',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headline5,
-                );
-              } else {
-                return Text(
-                  'In ${(time.days + 1).toString()} Tagen ist es soweit!',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headline5,
-                );
-              }
+              return Text(
+                'Endlich ist es soweit!',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline5,
+              );
             }
           },
         ),
