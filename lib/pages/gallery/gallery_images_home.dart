0a7d5493a4edc480e9ab8fff20d23e2dd84a5a5a
Daniel Smith <smithdaniel@web.de>
2021-10-25 08:38:23 +0200

    Download images - Android working.

--- lib/pages/gallery/gallery_images_home.dart
+++ lib/pages/gallery/gallery_images_home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -27,6 +29,13 @@ class _GalleryState extends State<Gallery> {
   bool downloadHint = false;
   _GalleryState({this.galleryName, this.gallery});
 
+  String _message = "";
+  String _path = "";
+  String _size = "";
+  String _mimeType = "";
+  File _imageFile;
+  int _progress = 0;
+
   List<String> _galleryImages = [];
   List<String> _slideShow = [];
   int _imagesLength;
@@ -38,6 +47,11 @@ class _GalleryState extends State<Gallery> {
   @override
   void initState() {
     _loadImages();
+    ImageDownloader.callback(onProgressUpdate: (String imageId, int progress) {
+      setState(() {
+        _progress = progress;
+      });
+    });
     super.initState();
   }
 
@@ -147,17 +161,102 @@ class _GalleryState extends State<Gallery> {
         .replaceAll(
             "https://firebasestorage.googleapis.com:443/v0/b/marry-me-cf187.appspot.com/o/",
             "")
+        .replaceAll(
+            "https://firebasestorage.googleapis.com/v0/b/marry-me-cf187.appspot.com/o/",
+            "")
         .replaceAll("%2F", "/");
 
     String _result = _finalUrl.substring(0, _finalUrl.indexOf('?alt=media'));
-
     await _returnStorageImage.getImageURL(_result).then((_value) {
       _finalDownload(_value);
     });
   }
 
-  void _finalDownload(String _value) async {
+  /*void _finalDownload(String _value) async {
     await ImageDownloader.downloadImage(_value);
+    setState(() {
+      downloadInProgress = false;
+    });
+    _showSnackbar();
+  }*/
+
+  Future<void> _finalDownload(
+    String url, {
+    AndroidDestinationType destination,
+    bool whenError = false,
+    String outputMimeType,
+  }) async {
+    print(url);
+    String fileName;
+    String path;
+    int size;
+    String mimeType;
+    try {
+      String imageId;
+
+      if (whenError) {
+        imageId = await ImageDownloader.downloadImage(url,
+                outputMimeType: outputMimeType)
+            .catchError((error) {
+          if (error is PlatformException) {
+            String path = "";
+            if (error.code == "404") {
+              print("Not Found Error.");
+            } else if (error.code == "unsupported_file") {
+              print("UnSupported FIle Error.");
+              path = error.details["unsupported_file_path"];
+            }
+            setState(() {
+              _message = error.toString();
+              _path = path ?? '';
+            });
+          }
+
+          print(error);
+        }).timeout(Duration(seconds: 10), onTimeout: () {
+          print("timeout");
+          return;
+        });
+      } else {
+        if (destination == null) {
+          imageId = await ImageDownloader.downloadImage(
+            url,
+            outputMimeType: outputMimeType,
+          );
+        } else {
+          imageId = await ImageDownloader.downloadImage(
+            url,
+            destination: destination,
+            outputMimeType: outputMimeType,
+          );
+        }
+      }
+
+      if (imageId == null) {
+        return;
+      }
+      fileName = await ImageDownloader.findName(imageId);
+      path = await ImageDownloader.findPath(imageId);
+      size = await ImageDownloader.findByteSize(imageId);
+      mimeType = await ImageDownloader.findMimeType(imageId);
+    } on PlatformException catch (error) {
+      setState(() {
+        _message = error.message ?? '';
+      });
+      return;
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      var location = Platform.isAndroid ? "Directory" : "Photo Library";
+      _message = 'Saved as "$fileName" in $location.\n';
+      _size = 'size:     $size';
+      _mimeType = 'mimeType: $mimeType';
+      _path = path ?? '';
+      return;
+    });
+
     setState(() {
       downloadInProgress = false;
     });
